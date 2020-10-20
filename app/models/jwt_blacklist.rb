3d40d0ba7c0af6fc83493ae7ea0aaa8a09eb57f4
tantch <ei12019@fe.up.pt>
2020-10-20 18:47:32 +0100

    blacklist change

--- app/models/jwt_blacklist.rb
+++ app/models/jwt_blacklist.rb
@@ -1,5 +1,6 @@
 class JWTBlacklist < ApplicationRecord
-  include Devise::JWT::RevocationStrategies::Blacklist
+  include Devise::JWT::RevocationStrategies::Denylist 
+  
 
   self.table_name = 'jwt_blacklist'
 end
